ac6955fd7a30558801b9e7af6d830f8de8ceb32b
Jorge <jorgeperez793@gmail.com>
2023-01-29 18:26:40 +0100

    Script prueba.sh

---
+++ dsljob.groovy
@@ -0,0 +1,6 @@
+job ('groovy-job-DSL') {
+
+description('Job DSL por groovy desde github')
+
+
+}
---
+++ prueba.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+echo "Ejecucion de prueba ssh despues de hacer un push local aklsdjaskdjl"
+echo "Bienvenido jenkins"
+
+for i in 1 2 3 4 5
+do
+    echo "welcome $i times"
+done
